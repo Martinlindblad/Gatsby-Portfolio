7eb410e2e1fd07d1315b58f1ca7ae647f9e2520b
MartinLindblad <martin.lindblad1@gmail.com>
2019-09-14 23:10:39 +0200

    bruh

--- src/Components/Layouts/index.js
+++ src/Components/Layouts/index.js
@@ -1,6 +1,7 @@
-import React from "./node_modules/react"
+import React from "react"
+
 export default ({ children }) => (
-  <div style={{ margin: `0 auto`, maxWidth: 650, padding: `0 1rem` }}>
+  <div style={{ margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` }}>
     {children}
   </div>
 )
