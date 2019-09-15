c03ff4c37f042cd98cbfa587568c288b3c88e526
MartinLindblad <martin.lindblad1@gmail.com>
2019-09-15 22:45:08 +0200

    landingPage almost done

--- src/pages/LandingPage/index.js
+++ src/pages/LandingPage/index.js
@@ -20,21 +20,46 @@ const LandingPage = () => (
   <div className="wrapper">
 <h1 className="ml8">
   <span className="letters-container">
-    <span className="letters letters-left">Hi</span>
-    <span className="letters bang">!</span>
+    <span className="letters letters-left">M<span className="letters bang">真灯</span>L</span>
+   
   </span>
   <span className="circle circle-white"></span>
   <span className="circle circle-dark"></span>
   <span className="circle circle-container"><span className="circle circle-dark-dashed"></span></span>
 </h1>
 
-    <div className="wave"></div>
-    <div className="wave"></div>
-    <div className="wave"></div>
-    <div className="wave"></div>
-    <div className="wave"></div>
+<div className="wave"></div>
+<div className="wave"></div>
+<div className="wave"></div>
+<div className="wave"></div>
+<div className="wave"></div>
+<div className="wave-two"></div>
+<div className="wave-two"></div>
+<div className="wave-two"></div>
+<div className="wave-two"></div>
+<div className="wave-two"></div>
+
+
   </div>
 )
+anime.timeline({loop: true})
+.add({
+    targets: '.wave',
+    scale: [0, 1],
+    opacity: 0,
+    delay: anime.stagger(1000),
+    easing: "easeInOutExpo",
+    duration: 6000,
+});
+anime.timeline({loop: true})
+.add({
+    targets: '.wave-two',
+    scale: [0, 1],
+    opacity: 0,
+    delay: anime.stagger(500),
+    easing: "easeInOutExpo",
+    duration: 6000,
+});
 anime.timeline({loop: true})
   .add({
     targets: '.ml8 .circle-white',
@@ -69,10 +94,19 @@ anime.timeline({loop: true})
   }).add({
     targets: '.ml8',
     opacity: 0,
-    duration: 1000,
+    duration: 1100,
     easing: "easeOutExpo",
-    delay: 1400
+    delay: 1500
+  
   });
+
+anime({
+  targets: '.ml8 .circle-dark-dashed',
+  rotateZ: 360,
+  duration: 8000,
+  easing: "linear",
+  loop: true
+});
 
 
 
